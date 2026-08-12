1b518f104fbbaa8bca96c7b55e948866e7b07c3b
VikramKumar-1 <vikuraj209@gmail.com>
2026-08-12 16:33:30 +0530

    Allow explicit image domains in next config for Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.ap-south-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'store4riders.s3.ap-south-2.amazonaws.com',
+      }
     ],
   },
 };
